b72efda481f1f3e6cf728e2c49ec1f7c092eddee
Sergei Zelinsky <zelinsky@lun.ua>
2017-08-02 13:55:27 +0300

    src/index.js: remove unnecessary checks, because of new production template for SSR version

--- src/index.js
+++ src/index.js
@@ -7,19 +7,9 @@ import { AsyncComponentProvider } from 'react-async-component';
 import asyncBootstrapper from 'react-async-bootstrapper';
 import './index.css';
 
-/* TODO: find better way for the next two checks
-  Next two checks are used to prevent passing strings like `{{INITIAL_STATE}}`
-  and '{{ASYNC_COMPONENTS_STATE}}' as redux-store initialState and
-  react-async-component rehydrateState if user loads application
-  without server-side rendering
- */
-const initialState = typeof window.INITIAL_STATE === 'object'
-  ? window.INITIAL_STATE
-  : {};
+const initialState = window.INITIAL_STATE;
 
-const rehydrateState = typeof window.ASYNC_COMPONENTS_STATE === 'object'
-  ? window.ASYNC_COMPONENTS_STATE
-  : {};
+const rehydrateState = window.ASYNC_COMPONENTS_STATE;
 
 const store = configureStore(initialState);
 
