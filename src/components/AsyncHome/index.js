5c6f406e3ec6401924f80267194e8b459647d4a5
Sergei Zelinsky <zelinsky@lun.ua>
2017-08-02 14:04:50 +0300

    use recomended structure of async components directories

--- src/components/AsyncHome/index.js
+++ src/components/AsyncHome/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {asyncComponent} from 'react-async-component';
 
 export default asyncComponent({
-  resolve: () => import('components/Home'),
+  resolve: () => import('./Home'),
   LoadingComponent: ({ match }) => <div>Resolving {match.url}</div>
 })
